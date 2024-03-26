a03743cd717276b6dc1bd1e9157d09b33d9f3d25
adrian-gendrano-dlsu <133202228+adrian-gendrano-dlsu@users.noreply.github.com>
2024-03-26 20:29:10 +0800

    Add files via upload

--- app/src/main/java/com/example/worktracker/MainActivity.java
+++ app/src/main/java/com/example/worktracker/MainActivity.java
@@ -8,17 +8,18 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
     // Comment to make login-free
-    FirebaseAuth auth;
-    FirebaseUser user;
+//    FirebaseAuth auth;
+//    FirebaseUser user;
 
-    Button button;
+    Button logoutBtn;
+    Button productListBtn;
+    Button activityTrackerBtn;
     TextView textViewEmail;
     TextView textViewName;
     List<String> data = new ArrayList<String>();
@@ -33,24 +34,38 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.main_activity);
 
         // Comment to make login-free
-        auth = FirebaseAuth.getInstance();
-        user = auth.getCurrentUser();
+//        auth = FirebaseAuth.getInstance();
+//        user = auth.getCurrentUser();
 
-        button = findViewById(R.id.logout);
+        logoutBtn = findViewById(R.id.logout);
+        productListBtn = findViewById(R.id.productListBtn);
+        activityTrackerBtn = findViewById(R.id.activityTrackerBtn);
         textViewEmail = findViewById(R.id.userEmail);
         textViewName = findViewById(R.id.userName);
 
         // Comment to make login-free
-        if (user == null) {
-            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+//        if (user == null) {
+//            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+//            startActivity(intent);
+//            finish();
+//        } else {
+//            textViewEmail.setText(user.getEmail());
+//            //FirebaseDatabase.getInstance().getReference();
+//       }
+
+        productListBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), PLPageActivity.class);
             startActivity(intent);
             finish();
-        } else {
-            textViewEmail.setText(user.getEmail());
-            //FirebaseDatabase.getInstance().getReference();
-       }
+        });
 
-        button.setOnClickListener(v -> {
+        activityTrackerBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), ActivityTrackActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        logoutBtn.setOnClickListener(v -> {
             FirebaseAuth.getInstance().signOut();
             Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
             startActivity(intent);
